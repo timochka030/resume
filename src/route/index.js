3a882345110bda55cd42ff0b0ef273dcedd20b6b
timochka030 <timofijkisil030@gmail.com>
2023-07-28 12:13:42 +0300

    bootstrap3

--- src/route/index.js
+++ src/route/index.js
@@ -11,32 +11,32 @@ var header = {
     lastname: 'Ivanov',
     position: 'Junior Fullstack JS Developer',
     salary: '600 $ в місяць',
-  }, 
-};
+  },
+}
 
 var footer = {
   social: {
     email: {
-      text: "dmytro@mail.com",
-      href: "https://www.linkedin.com/in/dmytro-test",
+      text: 'dmytro@mail.com',
+      href: 'https://www.linkedin.com/in/dmytro-test',
       address: 'Ukraine, Lutsk',
     },
-    
+
     phone: {
-      text: "+380670000123",
-      href: "tel:+380670000123",
+      text: '+380670000123',
+      href: 'tel:+380670000123',
     },
-   
+
     LinkedIn: {
-      text: "LinkedIn",
-      href: "https://www.linkedin.com/in/dmytro-test",
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
     },
 
     salary: {
-      text: "600$ в місяць"
+      text: '600$ в місяць',
     },
   },
-};
+}
 
 // ================================================================
 
@@ -59,7 +59,7 @@ router.get('/summary', function (req, res) {
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
     title: 'Resume',
-    
+
     page: {
       title: 'Resume | Summary',
     },
@@ -68,347 +68,333 @@ router.get('/summary', function (req, res) {
 
     main: {
       summary: {
-        title: "Summary",
-        text: "Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.",
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
       },
 
       experience: {
-        title: "Other experience",
-        text: "Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.",
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
       },
     },
 
     footer,
   })
 }),
+  // ================================================================
 
-// ================================================================
+  //              ↙ тут вводимо шлях (PATH) до сторінки
+  router.get('/skills', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('skills', {
+      // ↙ сюди вводимо JSON дані
+      title: 'Skils',
 
+      page: {
+        title: 'Resume | Skills',
+      },
 
-//              ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/skills', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('skills', {
-    // ↙ сюди вводимо JSON дані
-    title: 'Skils',
+      header,
 
-    page: {
-      title: 'Resume | Skills',
-    },
-    
-    header,
+      main: {
+        title: 'My hobbies',
+        titler: 'Navchannya',
 
-    main: {
-      title: 'My hobbies',
-      titler: 'Navchannya',
-
-      
-      skills: [
-        {
-          name: 'HTML',
-          point: 10,
-          isTop: true,
-        },
-        {
-          name: 'Handlebars',
-          point: 10,
-          isTop: true,
-        },
-        {
-          name: 'JS',
-          point: 8,
-          isTop: false,
-        },
-        {
-          name: 'VS Code & NPM',
-          point: 9,
-          isTop: false,
-        },
-        {
-          name: 'Git & Terminal',
-          point: 7,
-          isTop: false,
-        },
-        {
-          name: 'React.js',
-          point: 0,
-          isTop: false,
-        },
-        {
-          name: 'PHP',
-          point: null,
-          isTop: false,
-        },
-      ],
-
-      hobbies: [
-        {
-          name: 'football',
-          isMain: true,
-        },
-        {
-          name: 'Piano',
-          isMain: false,
-        },
-        {
-          name: 'IT',
-          isMain: true,
-        },
-        {
-          name: 'Play games that my frainds',
-          isMain: false,
-        },
-      ],
-    },
-
-    footer,
-  })
-}),
-
-
-//              ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/education', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('education', {
-    // ↙ сюди вводимо JSON дані
-    layout: 'big',
-
-    title: 'Education',
-    
-    page: {
-      title: 'Resume | Edukation',
-    },
-
-    header,
-
-  
-
-    main: {
-
-      title: `My certificates`,
-      educations: [
-        {
-          name: 'Все-освіта',
-          isEnd: false,
-        },
-        {
-          name: 'Укр-школа',
-          isEnd: true,
-        },
-        {
-          name: 'Луцький ліцей',
-          isEnd: true,
-        },
-        {
-          name: 'Школа №342',
-          isEnd: false,
-        },
-        {
-          name: 'Школа №586',
-          isEnd: true,
-        },
-      ],
-
-      certificates: [
-        {
-          name: 'Kartoshka',
-          id: '234567'
-        },
-        {
-          name: 'Pomidor',
-          id: '986740'
-        },
-        {
-          name: 'Yablochko',
-          id: '243868'
-        },
-      ],
-    },
-
-    footer,
-  })
-}),
-
-
-//              ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/work', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('work', {
-    // ↙ сюди вводимо JSON дані
-    layout: 'big',
-    
-    page: {
-      title: 'Resume | Work',
-    },
-
-    header,
-
-  
-
-    main: {
-      works: [
-        {
-          position: 'junior fullstack developer',
-          company: {
-            name: 'IT Brains',
-            url: null,
+        skills: [
+          {
+            name: 'HTML',
+            point: 10,
+            isTop: true,
           },
-          duration: {
-            from: '10.10.2022',
-            to: null,
+          {
+            name: 'Handlebars',
+            point: 10,
+            isTop: true,
           },
+          {
+            name: 'JS',
+            point: 8,
+            isTop: false,
+          },
+          {
+            name: 'VS Code & NPM',
+            point: 9,
+            isTop: false,
+          },
+          {
+            name: 'Git & Terminal',
+            point: 7,
+            isTop: false,
+          },
+          {
+            name: 'React.js',
+            point: 0,
+            isTop: false,
+          },
+          {
+            name: 'PHP',
+            point: null,
+            isTop: false,
+          },
+        ],
 
-          projectAmount: 3,
-          
+        hobbies: [
+          {
+            name: 'football',
+            isMain: true,
+          },
+          {
+            name: 'Piano',
+            isMain: false,
+          },
+          {
+            name: 'IT',
+            isMain: true,
+          },
+          {
+            name: 'Play games that my frainds',
+            isMain: false,
+          },
+        ],
+      },
 
-          projects: [
-            {
-              stackAmount: 4,
-              awardAmount: 2,
+      footer,
+    })
+  }),
+  //              ↙ тут вводимо шлях (PATH) до сторінки
+  router.get('/education', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('education', {
+      // ↙ сюди вводимо JSON дані
+      layout: 'big',
 
-              name: 'Resume',
-              about: 'process.processTicksAndRejections',
-              url: 'https://resume.com.ua/',
-              stacks: [
-                {
-                  name: 'React.js',
-                },
-                {
-                  name: 'HTML / CSS',
-                },
-                {
-                  name: 'Coder',
-                },
-                {
-                  name: 'Node.js',
-                },
-              ],
+      title: 'Education',
 
-              awards: [
-                {
-                  name: 'Cache boolean hinting to the engine it should cache',
-                },
-                {
-                  name: 'WebSocketServer.emit',
-                },
-              ],
+      page: {
+        title: 'Resume | Edukation',
+      },
+
+      header,
+
+      main: {
+        title: `My certificates`,
+        educations: [
+          {
+            name: 'Все-освіта',
+            isEnd: false,
+          },
+          {
+            name: 'Укр-школа',
+            isEnd: true,
+          },
+          {
+            name: 'Луцький ліцей',
+            isEnd: true,
+          },
+          {
+            name: 'Школа №342',
+            isEnd: false,
+          },
+          {
+            name: 'Школа №586',
+            isEnd: true,
+          },
+        ],
+
+        certificates: [
+          {
+            name: 'Kartoshka',
+            id: '234567',
+          },
+          {
+            name: 'Pomidor',
+            id: '986740',
+          },
+          {
+            name: 'Yablochko',
+            id: '243868',
+          },
+        ],
+      },
+
+      footer,
+    })
+  }),
+  //              ↙ тут вводимо шлях (PATH) до сторінки
+  router.get('/work', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('work', {
+      // ↙ сюди вводимо JSON дані
+      layout: 'big',
+
+      page: {
+        title: 'Resume | Work',
+      },
+
+      header,
+
+      main: {
+        works: [
+          {
+            position: 'junior fullstack developer',
+            company: {
+              name: 'IT Brains',
+              url: null,
             },
-          ],
-        },
-      ],
-    },
+            duration: {
+              from: '10.10.2022',
+              to: null,
+            },
 
-    footer,
-  })
-}),
+            projectAmount: 3,
 
+            projects: [
+              {
+                stackAmount: 4,
+                awardAmount: 2,
 
+                name: 'Resume',
+                about: 'process.processTicksAndRejections',
+                url: 'https://resume.com.ua/',
+                stacks: [
+                  {
+                    name: 'React.js',
+                  },
+                  {
+                    name: 'HTML / CSS',
+                  },
+                  {
+                    name: 'Coder',
+                  },
+                  {
+                    name: 'Node.js',
+                  },
+                ],
 
-
-// ================================================================
-
-//              ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/person', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('person', {
-    layout: 'person',
-    
-    person: {
-      name: 'Emma Johnson',
-      age: 32,
-      gender: 'Female',
-      address: {
-        street: '123 Main St',
-        city: 'New York',
-        state: 'NY',
-        zip: '10001',
-        country: 'USA',
+                awards: [
+                  {
+                    name: 'Cache boolean hinting to the engine it should cache',
+                  },
+                  {
+                    name: 'WebSocketServer.emit',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       },
-      education: [
-        {
-          degree: 'Bachelor of Science',
-          major: 'Computer Science',
-          university:
-            'Massachusetts Institute of Technology',
-          graduationYear: 2012,
+
+      footer,
+    })
+  }),
+  // ================================================================
+
+  //              ↙ тут вводимо шлях (PATH) до сторінки
+  router.get('/person', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('person', {
+      layout: 'person',
+
+      person: {
+        name: 'Emma Johnson',
+        age: 32,
+        gender: 'Female',
+        address: {
+          street: '123 Main St',
+          city: 'New York',
+          state: 'NY',
+          zip: '10001',
+          country: 'USA',
         },
-      ],
-      workExperience: [
-        {
-          company: 'Google',
-          title: 'Software Engineer',
-          startDate: '2012-06-01',
-          endDate: '2016-12-31',
-          responsibilities: [
-            'Developed new features for Google Maps',
-            'Worked on improving search algorithms',
-          ],
-          year_founded: 1990,
-          industry: 'Technology',
-          employees: [
-            {
-              name: 'John Smith',
-              position: 'CEO',
-              department: 'Executive',
-              projects: [
-                {
-                  name: 'Project Alpha',
-                  description:
-                    'Developing new software platform',
-                  status: 'In Progress',
-                  teams: [
-                    {
-                      team_name: 'Awesome Team',
-                      team_leader: {
-                        name: 'John Smith',
-                        title: 'Team Leader',
-                        email: 'john.smith@example.com',
-                      },
-                      team_members: [
-                        {
-                          name: 'Alice Johnson',
-                          title: 'Software Engineer',
-                          email:
-                            'alice.johnson@example.com',
-                          skills: ['Java', 'Python', 'SQL'],
-                          projects: [
-                            {
-                              name: 'Project A',
-                              description:
-                                'Lorem ipsum dolor sit amet',
-                              technologies: [
-                                'Java',
-                                'Spring Framework',
-                              ],
-                              team_members: [
-                                {
-                                  name: 'Bob Lee',
-                                  title:
-                                    'Software Engineer',
-                                },
-                                {
-                                  name: 'Cindy Chen',
-                                  title: 'UI Designer',
-                                },
-                              ],
-                            },
-                          ],
+        education: [
+          {
+            degree: 'Bachelor of Science',
+            major: 'Computer Science',
+            university:
+              'Massachusetts Institute of Technology',
+            graduationYear: 2012,
+          },
+        ],
+        workExperience: [
+          {
+            company: 'Google',
+            title: 'Software Engineer',
+            startDate: '2012-06-01',
+            endDate: '2016-12-31',
+            responsibilities: [
+              'Developed new features for Google Maps',
+              'Worked on improving search algorithms',
+            ],
+            year_founded: 1990,
+            industry: 'Technology',
+            employees: [
+              {
+                name: 'John Smith',
+                position: 'CEO',
+                department: 'Executive',
+                projects: [
+                  {
+                    name: 'Project Alpha',
+                    description:
+                      'Developing new software platform',
+                    status: 'In Progress',
+                    teams: [
+                      {
+                        team_name: 'Awesome Team',
+                        team_leader: {
+                          name: 'John Smith',
+                          title: 'Team Leader',
+                          email: 'john.smith@example.com',
                         },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
+                        team_members: [
+                          {
+                            name: 'Alice Johnson',
+                            title: 'Software Engineer',
+                            email:
+                              'alice.johnson@example.com',
+                            skills: [
+                              'Java',
+                              'Python',
+                              'SQL',
+                            ],
+                            projects: [
+                              {
+                                name: 'Project A',
+                                description:
+                                  'Lorem ipsum dolor sit amet',
+                                technologies: [
+                                  'Java',
+                                  'Spring Framework',
+                                ],
+                                team_members: [
+                                  {
+                                    name: 'Bob Lee',
+                                    title:
+                                      'Software Engineer',
+                                  },
+                                  {
+                                    name: 'Cindy Chen',
+                                    title: 'UI Designer',
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    })
   })
-})
 
 // ================================================================
-
 
 router.get('/js', function (req, res) {
   // res.render генерує нам HTML сторінку
@@ -595,7 +581,6 @@ router.get('/car', function (req, res) {
 })
 
 // ================================================================
-
 
 // router.get Створює нам один ентпоїнт
 
@@ -855,7 +840,6 @@ router.get('/bootstrap', function (req, res) {
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('bootstrap', {
     layout: 'bootstrap',
-    
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
@@ -867,7 +851,17 @@ router.get('/bootstrap2', function (req, res) {
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('bootstrap2', {
     layout: 'bootstrap',
-    
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+// ================================================================
+
+router.get('/bootstrap3', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap3', {
+    layout: 'bootstrap',
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
