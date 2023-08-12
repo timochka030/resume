d213076fcd6ceacff9dc430f327daf32a67b6b9f
timochka030 <timofijkisil030@gmail.com>
2023-08-12 14:13:15 +0300

    done

--- src/route/index.js
+++ src/route/index.js
@@ -2496,5 +2496,16 @@ router.get('/bootcss', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 // ================================================================
+
+router.get('/bootcss2', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootcss2', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+// ================================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
